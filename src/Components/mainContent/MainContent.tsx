7d3f192206a907051e704dacc1fb39e7555d055e
ArunRaj2136 <sakearun2126@gmail.com>
2023-08-08 11:30:49 +0530

    layout version 2

--- src/Components/mainContent/MainContent.tsx
+++ src/Components/mainContent/MainContent.tsx
@@ -2,7 +2,15 @@ import React from "react";
 import "./index.scss";
 
 function MainContent() {
-  return <div className="main__container">MainContent</div>;
+  return (
+    <div className="main__container">
+      <header className="main__container--header">
+        <img src="" alt="" className="main__container--logo" />
+        <h1 className="main__container--heading"> My Cryptos</h1>
+      </header>
+      {/* component to show card */} card
+    </div>
+  );
 }
 
 export default MainContent;
--- src/Components/sidebar/Sidebar.tsx
+++ src/Components/sidebar/Sidebar.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 import "./index.scss";
+//
+
 const Sidebar = () => {
-  return <div className="sidebar__container">Sidebar</div>;
+  return (
+    <div className="sidebar__container">
+      <div className="sidebar__container--header">
+        <img src="" alt="left arrow" className="sidebar__container--logo" />
+        <h2 className="sidebar__container--heading">Cryptos (0)</h2>
+      </div>
+      <div className="sidebar__container--input">
+        <input type="text" placeholder="search" />
+      </div>
+      {/* List Card Component */}
+      CARD
+    </div>
+  );
 };
 
 export default Sidebar;
