99e8874d241fb70032a8083f34775eed45c48e62
ArunRaj2136 <sakearun2126@gmail.com>
2023-08-08 11:46:42 +0530

    controlled input

--- src/Components/sidebar/Sidebar.tsx
+++ src/Components/sidebar/Sidebar.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.scss";
 //
 
 const Sidebar = () => {
+  const [searchTerm, setSearchTerm] = useState<string>("");
   return (
     <div className="sidebar__container">
       <div className="sidebar__container--header">
@@ -10,7 +11,14 @@ const Sidebar = () => {
         <h2 className="sidebar__container--heading">Cryptos (0)</h2>
       </div>
       <div className="sidebar__container--input">
-        <input type="text" placeholder="search" />
+        <input
+          type="text"
+          placeholder="search"
+          value={searchTerm}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setSearchTerm(e?.target?.value)
+          }
+        />
       </div>
       {/* List Card Component */}
       CARD
